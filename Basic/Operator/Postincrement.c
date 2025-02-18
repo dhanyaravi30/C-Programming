eff511f6f51e6c22ef88e92cbdaed7f3b98eefc8
dhanyaravi30 <dhanyafab@gmail.com>
2025-02-18 20:57:03 +0530

    Done

---
+++ Basic/Operator/Postincrement.c
@@ -0,0 +1,11 @@
+
+// Post-increment (a++): Increases a after its value is used.
+
+#include<stdio.h>
+int main(){
+    int a =2;
+    int b = a++;
+
+    printf("%d  \n", a);
+    printf("%d", b);
+}
---
+++ Basic/Operator/Preincrement.c
@@ -0,0 +1,11 @@
+// Pre-increment (++a): Increases a before its value is used
+
+#include<stdio.h>
+int main(){
+    int a =2;
+    int b = ++a;
+
+    printf(" %d  \n", a);
+    printf(" %d ", b);
+
+}
