42dcf6f064c59201b8860b350d48eedd0a5977ac
dhanyaravi30 <dhanyafab@gmail.com>
2025-02-18 20:24:11 +0530

    Done

---
+++ Basic/Integer/Integer.c
@@ -0,0 +1,6 @@
+// program to print the integer value
+#include<stdio.h>
+int main(){
+    int a = 2;
+    printf("%d",a);
+}
---
+++ Basic/Integer/ReadInteger.c
@@ -0,0 +1,11 @@
+//  C program to read integer values in C:
+
+#include<stdio.h>
+int main(){
+    int num;
+    
+    printf("Enter the number: ");
+    scanf("%d", &num);
+
+    printf("The number is %d", num);
+}
