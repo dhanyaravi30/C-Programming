2c8b54f456bd5612324c432bd1980a88bb46f9aa
dhanyaravi30 <dhanyafab@gmail.com>
2025-02-18 20:34:00 +0530

    Done

---
+++ Basic/Addition/Addiionconsole.c
@@ -0,0 +1,17 @@
+// Values get from the console
+
+
+#include<stdio.h>
+int main(){
+    int a,b,sum=0;
+
+    printf("Enter the value of a: ");
+    scanf("%d", &a);
+
+    printf("Enter the value of b: ");
+    scanf("%d", &b);
+
+    sum=a+b;
+
+    printf("The sum is %d", sum);
+}
---
+++ Basic/Addition/Simpleadd.c
@@ -0,0 +1,9 @@
+//Simple Addition as defined  in the program itself
+
+#include<stdio.h>
+int main(){
+    int a=2;
+    int b=3;
+    int sum = a+b;
+    printf("The sum is %d", sum);
+}
